734f245dbcf832590498f6ecde65c18d1723b6c9
Balogh Csaba <cibito.kun@gmail.com>
2019-11-05 17:04:15 +0100

    asdasd

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <title>GRID</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+    </body>
+</html>
