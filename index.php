128adac54bf0e4366cc86e912db89c087575bccb
Balogh Csaba <cibito.kun@gmail.com>
2019-11-05 17:27:56 +0100

    fdg

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
     <head>
         <title>GRID</title>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
     </head>
     <body>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    
+  
     </body>
 </html>
